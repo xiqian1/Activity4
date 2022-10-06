9250508ebfc29ec577fd47fd303f817ff908c848
xiqian1 <keviny413@gmail.com>
2022-10-05 21:22:35 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        interface MyInterface
+        {
+            string StudentName { get; set; }
+        }
+
+
+        class User<T>
+        {
+            private T[] arr = new T[100];
+            public T this[int i]
+            {
+                get { return arr[i]; }
+                set { arr[i] = value; }
+            }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                var stringCollection = new User<string>();
+                stringCollection[0] = "Xiqian";
+                Console.WriteLine(stringCollection[0]);
+            }
+        }
+    }
+}
